bc6d291e74e1e9ba8cf5908be4fc564bd6a6e82a
StianSka <116058912+StianSka@users.noreply.github.com>
2023-06-28 14:25:52 +0200

    Update HeroStatScreen.jsx

--- react-prosject/src/components/HeroStatScreen.jsx
+++ react-prosject/src/components/HeroStatScreen.jsx
@@ -6,9 +6,6 @@ export default function HeroStatScreen({ hero }) {
         ""
       ) : (
         <>
-          <div className="hero-stats">
-            Hp: {hero.currentHp} / {hero.maxHp}
-          </div>
           <div className="hero-stats">Potions: {hero.potionCount}</div>
           <div className="hero-stats">Strength: {hero.strength}</div>
           <div className="hero-stats">Speed: {hero.speed}</div>
